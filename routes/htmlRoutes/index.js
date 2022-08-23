6b5d83c061d9aeb31434265accfbebd270c89e3f
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-08-22 21:26:39 -0400

    htmlRoutes added

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,1 +1,17 @@
 const router = require('express').Router();
+
+//  points to the root route of the server
+router.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+// It will redirect the user if the path does not exist 
+router.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+module.exports = router;
