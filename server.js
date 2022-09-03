9a9aa53e5f708f82a981d05719eec3f1bcfec1ed
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-09-03 12:57:06 -0400

    port 3001 bug fixed, deleted html and notes routes files

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
-const apiRoutes = require('./routes/apiRoutes/notes');
-const htmlRoutes = require('./routes/htmlRoutes');
 const express = require('express');
-const { db } = require('./db/db.json');
+const path = require('path');
+const fs = require('fs');
 
-//const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 // Middleware helper functions
 
 app.use(express.static('public'));
-app.use(express.urlencoded({esteended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/',htmlRoutes);
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+
+//  points to the root route of the server
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// It will redirect the user if the path does not exist 
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db.db.json'));
+})
+
+app.post('/api/routes', (req,res) => {
+    let note = req.body;
+    let list = JSON(parse(fs.readFileSync('/db/db.json', 'utf8')));
+    let lengthNote = list.length.toString();
+
+    newNote.id = lengthNote;
+    list.push(newNote);
+
+    fs.writeFileSync('/db/db.json', JSON.stringify(list));
+    res.json(list);
+})
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
 })
